cc424d06da28f9090f790bb36bf59de8c8a2622f
ivandjuka77 <captainsubzer@gmail.com>
2023-06-29 19:03:44 +0200

    Commit

--- components/AddContact.tsx
+++ components/AddContact.tsx
@@ -20,7 +20,7 @@ import {
 
 const AddContact = ({ contacts, jobId }: any) => {
     console.log(contacts, 'contactssssss');
-    console.log(jobId, 'jobId');
+    console.log(jobId, 'jobId2');
 
     const [open, setOpen] = useState(false);
     const [closed, setClosed] = useState(false);
