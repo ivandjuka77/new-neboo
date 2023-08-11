1dabc271640ace07e404d87fb958ade8949de5a7
ivandjuka77 <captainsubzer@gmail.com>
2023-08-11 16:47:30 +0200

    Layout changes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 >
                     <ThemeProvider
                         attribute="class"
-                        defaultTheme="system"
+                        defaultTheme="dark"
                         enableSystem
                     >
                         <div className="relative flex min-h-screen flex-col">
